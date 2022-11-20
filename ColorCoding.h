9af2b6d591f89f1e1f489ff4f0f00fa99996b59d
PoonkuzhaliGithub <115685368+PoonkuzhaliGithub@users.noreply.github.com>
2022-11-20 11:51:04 +0530

    Create ColorCoding.h

---
+++ ColorCoding.h
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+
+int numberOfMajorColors =    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
+int numberOfMinorColors =    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
