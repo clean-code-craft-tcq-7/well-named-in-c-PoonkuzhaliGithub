6dfb11c9b6e8659f618b6ffed3c0d3b35b100342
PoonkuzhaliGithub <115685368+PoonkuzhaliGithub@users.noreply.github.com>
2022-11-20 13:02:50 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,9 +4,7 @@
 
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 
-void testNumberToPair(int pairNumber,
-    enum MajorColor expectedMajor,
-    enum MinorColor expectedMinor)
+void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor)
 {
     ColorPair colorPair = GetColorFromPairNumber(pairNumber);
     char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
@@ -16,10 +14,7 @@ void testNumberToPair(int pairNumber,
     assert(colorPair.minorColor == expectedMinor);
 }
 
-void testPairToNumber(
-    enum MajorColor major,
-    enum MinorColor minor,
-    int expectedPairNumber)
+void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber)
 {
     ColorPair colorPair;
     colorPair.majorColor = major;
@@ -30,11 +25,19 @@ void testPairToNumber(
 }
 
 int main() {
+
+    ColorPair colorPair;
+    printf("PRINTING MANUAL\n");
+    for(int i =1 ;i<26;i++)
+    {
+        colorPair = GetColorFromPairNumber(i);
+        printf("majorcolor-%s,minorcolor-%s,%d-pairNumber\n",MajorColorNames[colorPair.majorColor],MajorColorNames[colorPair.minorColor],i);
+    }
+    printf("DO TEST VERIFICATION\n");
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
 
     testPairToNumber(BLACK, ORANGE, 12);
     testPairToNumber(VIOLET, SLATE, 25);
-
     return 0;
 }
