846bef4644622eedb10f2c571885a3fe806ea411
PoonkuzhaliGithub <115685368+PoonkuzhaliGithub@users.noreply.github.com>
2022-11-20 12:19:09 +0530

    Update ColorCoding.c

--- ColorCoding.c
+++ ColorCoding.c
@@ -4,6 +4,21 @@
 int numberOfMajorColors =    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 int numberOfMinorColors =    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
+
+const char* MajorColorNames[] = {
+    "White", "Red", "Black", "Yellow", "Violet"
+};
+
+const char* MinorColorNames[] = {
+    "Blue", "Orange", "Green", "Brown", "Slate"
+};
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+    sprintf(buffer, "%s %s",
+        MajorColorNames[colorPair->majorColor],
+        MinorColorNames[colorPair->minorColor]);
+}
+
 ColorPair GetColorFromPairNumber(int pairNumber) {
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
