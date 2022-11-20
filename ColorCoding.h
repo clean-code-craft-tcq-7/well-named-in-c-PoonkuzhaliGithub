7d08e08c6d432fa8cb23c95e91ed2239cc279597
PoonkuzhaliGithub <115685368+PoonkuzhaliGithub@users.noreply.github.com>
2022-11-20 13:32:37 +0530

    Update ColorCoding.h

--- ColorCoding.h
+++ ColorCoding.h
@@ -8,6 +8,9 @@ typedef struct {
     enum MinorColor minorColor;
 } ColorPair;
 
+const char* MajorColorNames[];
+const char* MinorColorNames[];
+
 void ColorPairToString(const ColorPair* colorPair, char* buffer);
 void testNumberToPair(int pairNumber, enum MajorColor expectedMajor, enum MinorColor expectedMinor);
 void testPairToNumber(enum MajorColor major, enum MinorColor minor, int expectedPairNumber);
