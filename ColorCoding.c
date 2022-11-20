af5193f54a7ddfa4687216456576476bbe7d82cf
PoonkuzhaliGithub <115685368+PoonkuzhaliGithub@users.noreply.github.com>
2022-11-20 12:20:07 +0530

    Update ColorCoding.c

--- ColorCoding.c
+++ ColorCoding.c
@@ -4,7 +4,6 @@
 int numberOfMajorColors =    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 int numberOfMinorColors =    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
-
 const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
